db17501183899d139f794931e1bc3c409e9230d9
Andrej <andrej_velevski@hotmail.com>
2020-11-28 14:53:51 +0100

    04.2 Kontrola na tekot na programite

--- C_Training/main.c
+++ C_Training/main.c
@@ -2,17 +2,19 @@
 #include "T02_Uslovi_i_relacioni_operatori.h"
 #include "T03_Osnovni_tipovi_na_podtoci.h"
 #include "T04_1_Kontrola_na_tekot_na_programite.h"
+#include "T04_2_Kontrola_na_tekot_na_programite.h"
 
 /*
     zadaca01_01, ... , zadaca01_06
     zadaca02_01
     zadaca03_01, ... , zadaca03_08
     zadaca04_1_01, ... , zadaca04_1_09
+    zadaca04_2_01, ... , zadaca04_2_05
 */
 
 int main()
 {
-    zadaca04_1_09();
+    zadaca04_2_04();
 
     return 0;
 }
